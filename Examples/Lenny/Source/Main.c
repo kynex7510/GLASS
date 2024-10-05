2cf1ed1e933147835318fb984900cfffa97e4ef4
kynex7510 <nikkitten3@gmail.com>
2024-10-05 22:54:13 +0200

    Use offsetof

--- Examples/Lenny/Source/Main.c
+++ Examples/Lenny/Source/Main.c
@@ -41,10 +41,10 @@ static GLuint sceneInit(void) {
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_VertexList), g_VertexList, GL_STATIC_DRAW);
 
     // Configure attributes for use with the vertex shader.
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL); // v0 = position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, x))); // v0 = position
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(Vertex) / 2)); // v1 = normal
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, nx))); // v1 = normal
     glEnableVertexAttribArray(1);
 
     return vbo;
