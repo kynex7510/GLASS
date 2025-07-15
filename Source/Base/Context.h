324bf2ab7b874f2fa726f5959c6cba217c5a21df
kynex7510 <nikkitten3@gmail.com>
2025-07-15 22:11:43 +0200

    Optimize context structure

--- Source/Base/Context.h
+++ Source/Base/Context.h
@@ -3,19 +3,20 @@
 
 #include "Base/Types.h"
 
+/*
 typedef struct {
     GLASSCtxParams params; // Context parameters.
     u32 flags;             // State flags.
     GLenum lastError;      // Actually first error.
 
-    /* Platform */
+    // Platform
     KYGXCmdBuffer GXCmdBuf; // GX command buffer.
 
-    /* Buffers */
+    // Buffers
     GLuint arrayBuffer;        // GL_ARRAY_BUFFER
     GLuint elementArrayBuffer; // GL_ELEMENT_ARRAY_BUFFER  
 
-    /* Framebuffer */
+    // Framebuffer
     GLuint framebuffer[2]; // Bound framebuffer object.
     GLuint renderbuffer;   // Bound renderbuffer object.
     u32 clearColor;        // Color buffer clear value.
@@ -23,30 +24,30 @@ typedef struct {
     u8 clearStencil;       // Stencil buffer clear value.
     bool block32;          // Block mode 32.
 
-    /* Viewport */
+    // Viewport
     GLint viewportX;   // Viewport X.
     GLint viewportY;   // Viewport Y.
     GLsizei viewportW; // Viewport width.
     GLsizei viewportH; // Viewport height.
 
-    /* Scissor */
+    // Scissor
     GPUScissorMode scissorMode; // Scissor test mode.
     GLint scissorX;             // Scissor box X.
     GLint scissorY;             // Scissor box Y.
     GLsizei scissorW;           // Scissor box W.
     GLsizei scissorH;           // Scissor box H.
 
-    /* Program */
+    // Program
     GLuint currentProgram; // Shader program in use.
     
-    /* Attributes */
+    // Attributes
     AttributeInfo attribs[GLASS_NUM_ATTRIB_REGS]; // Attributes data.
     GLsizei numEnabledAttribs;                    // Number of enabled attributes.
 
-    /* Fragment */
+    // Fragment
     GLenum fragMode; // Fragment mode.
 
-    /* Color, Depth */
+    // Color, Depth
     bool writeRed;    // Write red.
     bool writeGreen;  // Write green.
     bool writeBlue;   // Write blue.
@@ -55,19 +56,19 @@ typedef struct {
     bool depthTest;   // Depth test enabled.
     GLenum depthFunc; // Depth test function.
 
-    /* Depth Map */
+    // Depth Map
     GLclampf depthNear;    // Depth map near val.
     GLclampf depthFar;     // Depth map far val.
     bool polygonOffset;    // Depth map offset enabled.
     GLfloat polygonFactor; // Depth map factor (unused).
     GLfloat polygonUnits;  // Depth map offset units.
 
-    /* Early Depth */
+    // Early Depth
     bool earlyDepthTest;      // Early depth test enabled.
     GLclampf clearEarlyDepth; // Early depth clear value.
     GLenum earlyDepthFunc;    // Early depth function.
 
-    /* Stencil */
+    // Stencil
     bool stencilTest;        // Stencil test enabled.
     GLenum stencilFunc;      // Stencil function.
     GLint stencilRef;        // Stencil reference value.
@@ -77,17 +78,17 @@ typedef struct {
     GLenum stencilDepthFail; // Stencil pass, depth fail.
     GLenum stencilPass;      // Stencil pass + depth pass or no depth.
 
-    /* Cull Face */
+    // Cull Face
     bool cullFace;        // Cull face enabled.
     GLenum cullFaceMode;  // Cull face mode.
     GLenum frontFaceMode; // Front face mode.
 
-    /* Alpha */
+    // Alpha
     bool alphaTest;    // Alpha test enabled.
     GLenum alphaFunc;  // Alpha test function.
     GLclampf alphaRef; // Alpha test reference value.
 
-    /* Blend, Logic Op */
+    // Blend, Logic Op
     bool blendMode;       // Blend mode.
     u32 blendColor;       // Blend color.
     GLenum blendEqRGB;    // Blend equation RGB.
@@ -98,13 +99,84 @@ typedef struct {
     GLenum blendDstAlpha; // Blend destination alpha.
     GLenum logicOp;       // Logic op.
 
-    /* Texture */
+    // Texture
     GLuint textureUnits[GLASS_NUM_TEX_UNITS]; // Texture units.
     size_t activeTextureUnit;                 // Currently active texture unit.  
 
-    /* Combiners */
+    // Combiners
     GLint combinerStage;                               // Current combiner stage.
     CombinerInfo combiners[GLASS_NUM_COMBINER_STAGES]; // Combiners.
+} CtxCommon;
+ */
+
+typedef struct {
+    u32 flags;
+    GLenum lastError;
+    GLuint arrayBuffer;
+    GLuint elementArrayBuffer;
+    GLuint renderbuffer;
+    u32 clearColor;
+    GLint viewportX;
+    GLint viewportY;
+    GLsizei viewportW;
+    GLsizei viewportH;
+    GLint scissorX;
+    GLint scissorY;
+    GLsizei scissorW;
+    GLsizei scissorH;
+    GLuint currentProgram;
+    GLsizei numEnabledAttribs;
+    GLenum fragMode;
+    GLenum depthFunc;
+    GLenum earlyDepthFunc;
+    GLenum stencilFunc;
+    GLint stencilRef;
+    GLuint stencilMask;
+    GLuint stencilWriteMask;
+    GLenum stencilFail;
+    GLenum stencilDepthFail; 
+    GLenum stencilPass;
+    GLenum cullFaceMode;
+    GLenum frontFaceMode;
+    GLenum alphaFunc;
+    u32 blendColor;
+    GLenum blendEqRGB;
+    GLenum blendEqAlpha;
+    GLenum blendSrcRGB;
+    GLenum blendDstRGB;
+    GLenum blendSrcAlpha;
+    GLenum blendDstAlpha;
+    GLenum logicOp;
+    GLint combinerStage;
+    size_t activeTextureUnit;
+    GLuint framebuffer[2];
+    GLuint textureUnits[GLASS_NUM_TEX_UNITS];
+    KYGXCmdBuffer GXCmdBuf;
+    GLASSCtxParams params;
+    CombinerInfo combiners[GLASS_NUM_COMBINER_STAGES];
+    AttributeInfo attribs[GLASS_NUM_ATTRIB_REGS];
+    GLclampf clearDepth;
+    GPUScissorMode scissorMode;
+    GLclampf depthNear;
+    GLclampf depthFar;
+    GLfloat polygonFactor;
+    GLfloat polygonUnits;
+    GLclampf clearEarlyDepth;
+    GLclampf alphaRef;
+    u8 clearStencil;
+    bool block32;
+    bool writeRed;
+    bool writeGreen;
+    bool writeBlue;
+    bool writeAlpha;
+    bool writeDepth;
+    bool depthTest;
+    bool polygonOffset;
+    bool earlyDepthTest;
+    bool stencilTest;
+    bool cullFace;
+    bool alphaTest;
+    bool blendMode;
 } CtxCommon;
 
 void GLASS_context_initCommon(CtxCommon* ctx, const GLASSCtxParams* ctxParams);
